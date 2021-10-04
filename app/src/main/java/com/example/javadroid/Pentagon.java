49673cb1bc91cb5b44868c6352ff4a874adbfe0b
Jake <jsample@ualberta.ca>
2021-10-03 23:09:29 -0600

    Added Pentagon

--- app/src/main/java/com/example/javadroid/Pentagon.java
+++ app/src/main/java/com/example/javadroid/Pentagon.java
@@ -1,2 +1,18 @@
-package com.example.javadroid;public class Pentagon {
+package com.example.javadroid;
+
+public class Pentagon extends Shape{
+
+    private int side;
+
+    public Pentagon(int side){
+        this.side = side;
+    }
+
+    public int getSide() {
+        return side;
+    }
+
+    public void setSide(int side) {
+        this.side = side;
+    }
 }
