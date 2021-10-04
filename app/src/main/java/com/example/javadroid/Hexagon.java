9451131e4106660931357e724685e43b1fcc18a9
Jake <jsample@ualberta.ca>
2021-10-03 22:42:26 -0600

    Added Hexagon class

--- app/src/main/java/com/example/javadroid/Hexagon.java
+++ app/src/main/java/com/example/javadroid/Hexagon.java
@@ -1,2 +1,20 @@
-package com.example.javadroid;public class Hexagon {
-}
+package com.example.javadroid;
+
+public class Hexagon extends Shape{
+
+    private int side;
+
+    public Hexagon(int side){
+        this.side = side;
+    }
+
+    public int getSide(){
+        return side;
+    }
+
+    public void setSide(int side){
+        this.side = side;
+    }
+
+}//end Hexagon
+
