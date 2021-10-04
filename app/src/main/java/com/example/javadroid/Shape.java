5aca575eaf50e290a5a28ae74f900f4622542e59
“ayassin1” <yassin1@ualberta.ca>
2021-10-04 14:43:31 -0600

    Fixed color string conflict

--- app/src/main/java/com/example/javadroid/Shape.java
+++ app/src/main/java/com/example/javadroid/Shape.java
@@ -3,9 +3,5 @@ package com.example.javadroid;
 public abstract class Shape {
     private int x;
     private int y;
-<<<<<<< HEAD
     private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
-=======
-    private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
->>>>>>> a8f6ec6f00575ad109d8343ff6c4282cdd6f27c4
 }
